2fdaae71cd033d3110fe5c856967bead0b8ceca8
Alec Aivazis <alec@aivazis.com>
2023-03-26 23:08:13 -0700

    add api

---
+++ houdini.config.js
@@ -0,0 +1,9 @@
+/** @type {import('houdini').ConfigFile} */
+const config = {
+  schemaPath: "./server/schema.graphql",
+  plugins: {
+    "houdini-react": {},
+  },
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { path } from "houdini";
+import houdini from "houdini/vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [houdini(), react()],
+  // TODO: the vite plugin should do this
+  resolve: {
+    alias: {
+      $houdini: path.resolve(".", "/$houdini"),
+      "$houdini/*": path.resolve(".", "/$houdini", "*"),
+    },
+  },
 });
